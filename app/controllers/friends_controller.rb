f41f8f80ccc72f1b1c97704d7c42ab16ffc9d848
Jose Manuel <josemanuel@z1.digital>
2024-09-26 12:14:18 +0200

    feat: update and delete friend permissions

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,7 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: %i[ index show ]
+  before_action :is_friend_of_the_current_user, only: %i[ edit update destroy ]
 
   # GET /friends or /friends.json
   def index
@@ -12,7 +14,7 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+    @friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -21,7 +23,7 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
+    @friend = current_user.friends.build(friend_params)
 
     respond_to do |format|
       if @friend.save
@@ -67,5 +69,10 @@ class FriendsController < ApplicationController
   # Only allow a list of trusted parameters through.
   def friend_params
     params.require(:friend).permit(:first_name, :last_name, :phone, :twitter, :user_id)
+  end
+
+  def is_friend_of_the_current_user
+    @friend = current_user.friends.find_by(id: params[:id])
+    redirect_to friends_path, notice: "You can not modify this friend!" if @friend.nil?
   end
 end
